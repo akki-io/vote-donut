4419e5b1fac5037759465596afdec555726b9daa
Akki Khare <admin@akki.io>
2020-07-23 20:42:54 -0400

    inital commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getCandidate = /* GraphQL */ `
+  query GetCandidate($id: ID!) {
+    getCandidate(id: $id) {
+      id
+      name
+      image
+      description
+      votes
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listCandidates = /* GraphQL */ `
+  query ListCandidates(
+    $filter: ModelCandidateFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCandidates(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        image
+        description
+        votes
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+import '@/assets/css/tailwind.css'
+Amplify.configure(awsconfig)
 
 Vue.config.productionTip = false
 

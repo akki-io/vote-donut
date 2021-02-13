ea88525c6527e8329c34ac2a4751ef1a6037dc03
Akki Khare <admin@akki.io>
2021-02-13 09:07:51 -0500

    update dependencies, add footer and prettier.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Amplify from 'aws-amplify'
-import awsconfig from './aws-exports'
-import '@/assets/css/tailwind.css'
-Amplify.configure(awsconfig)
+import Vue from 'vue';
+import App from './App.vue';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import './tailwind.css';
+Amplify.configure(awsconfig);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount('#app');
